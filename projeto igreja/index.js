0fa5e6d5d8c604b9be002dd152f57a2c5f3a442d
leopac36 <leonardopacheco4105@gmail.com>
2024-04-08 16:39:34 -0300

    Initial commit

---
+++ projeto igreja/index.js
@@ -0,0 +1,10 @@
+function mouseOver ('1'){
+  document.getElementById("conteudo").innerHTML = ""
+}
+function mouseOut ('2'){
+  document.getElementById("conteudo").innerHTML = "conteudo"
+}
+function mouseClick ('3'){
+  document.getElementById("conteudo").innerHTML = "conteudo"
+}
+
