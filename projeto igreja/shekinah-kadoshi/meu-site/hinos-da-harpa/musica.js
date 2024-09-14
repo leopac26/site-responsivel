05b89009b285a5e091d9b054610a5e50c2864d83
leopac36 <161525860+leopac26@users.noreply.github.com>
2024-09-14 09:02:53 -0300

    atualizando

---
+++ projeto igreja/shekinah-kadoshi/meu-site/hinos-da-harpa/musica.js
@@ -0,0 +1,177 @@
+var tomAtual = 0;
+var ultimaMusica;
+function transformar(textoMusica) {
+    // (?:[A-G](?:b|bb)*(?:#|##|sus|maj|min|aug|m|M|\\+|-|dim)*[\d\/]*)*)(?=\s|$)(?! \w))
+
+    const notas = "[A-G]",
+    acentuacoes = "(?:b|bb)",
+    acordes = "*(?:#|##|sus|maj|min|aug|m|M|\\+|-|dim)",
+    com = "*[\\d\\/]*",
+    numeros = "*(?:[1-9])",
+    foraMusica = "(?=\\s|$)(?! \\w)",
+    padrao = "("+"\\b(" + notas + acentuacoes + numeros + acordes + com + '(?:' + notas + acentuacoes + acordes + com + ')*)'+foraMusica+")",
+    regex = new RegExp(padrao, "g");
+
+    let acordesTexto = textoMusica.split('[Acordes]')[1];
+    let novosAcordesSeparados = acordesTexto.split('\n')
+    let novosAcordes = [];
+    novosAcordesSeparados.forEach(linha => {
+        let sep = linha.split(' = ');
+        if(sep[0] != '')
+            novosAcordes.push({ n: sep[0], m: sep[1], d: null, p: null});
+    });
+    
+    biblioteca.setAcordesCustomizados(novosAcordes);    
+
+    let separadoPorLinhas = textoMusica.split('\n');
+
+    alterado = [];
+    const campo = document.getElementById('resultado');
+    campo.innerHTML = "";
+    separadoPorLinhas.forEach(linha => {
+        if(linha.match(regex)){
+            alterado.push(linha.replace(regex,"<b>$1</b>"));
+        }else{
+            alterado.push(linha);
+        }
+        
+    });
+    //monta titulo
+    let h2 = document.createElement('h2');
+    h2.innerHTML = alterado[0];
+    campo.appendChild(h2);
+    alterado.shift();
+    //monta canção
+    let pre = document.createElement('pre');
+    pre.innerHTML = alterado.join('\n');
+    campo.appendChild(pre);
+    function removerDaTela(element) {
+        if(element.target.querySelector('div')){
+            element.target.querySelector('div').remove();
+        }
+    }
+    document.querySelectorAll('#resultado > pre > b').forEach(element => {
+        element.addEventListener("mouseenter", function(element) {            
+            if(!element.target.querySelector('div')){                
+                let campoAcorde = document.createElement('div');
+                campoAcorde.style = "height: 140px; width: 110px; position:absolute; top: "+(element.target.offsetTop-140)+"px; left: "+(element.target.offsetLeft + (element.target.offsetWidth/2) - 55)+"px; background-color: transparent; z-index: 100;"     
+                let acorde = biblioteca.getAcordePorCifra(element.target.innerText);                
+                let novoAcorde = new Acorde(acorde.n, acorde.m, acorde.d,acorde.p);
+                campoAcorde.appendChild(novoAcorde.montaAcorde());
+                element.target.appendChild(campoAcorde);
+            }
+        });
+
+        element.addEventListener("mouseleave", removerDaTela);
+
+        element.addEventListener("click", function(element) {
+            if(this.getAttribute('tela') != 'true'){
+                this.setAttribute("tela", "true");
+                this.removeEventListener("mouseleave", removerDaTela, false);
+            } else {
+                this.setAttribute("tela", "null");
+                this.addEventListener("mouseleave", removerDaTela);
+            }
+        });
+    });
+};
+
+function transposicao(event) { // enviar 1 ou -1
+    let meioTom = (tomAtual < parseInt(event.target.value)) ? 1 : -1;
+    tomAtual = parseInt(event.target.value);    
+    document.querySelectorAll('#resultado > pre > b').forEach(element => {
+        // console.log(element.innerText);
+        let acorde = element.innerText;
+        let baixo = element.innerText.split('/');        
+        let numeroAtual = biblioteca.getNumeroBaseAcorde(acorde);
+        let numeroTranposto = numeroAtual + (meioTom);        
+        if(numeroTranposto > 12)
+            numeroTranposto = 1;
+        if(numeroTranposto < 1)
+            numeroTranposto = 12;        
+        let novaBase = biblioteca.getBaseAcordePorPosicao(numeroTranposto);
+        let novoAcorde = biblioteca.alterarBaseAcorde(novaBase,baixo[0]);
+        // alterar baixo
+        if(baixo.length > 1){
+            let numeroAtualBaixo = biblioteca.getNumeroBaseAcorde(baixo[1]);
+            let numeroTranpostoBaixo = numeroAtualBaixo + (meioTom);        
+            if(numeroTranpostoBaixo > 12)
+                numeroTranpostoBaixo = 1;
+            if(numeroTranpostoBaixo < 1)
+                numeroTranpostoBaixo = 12;  
+            let novaBaseBaixo = biblioteca.getBaseAcordePorPosicao(numeroTranpostoBaixo);
+            novoAcorde = novoAcorde + '/' + biblioteca.alterarBaseAcorde(novaBaseBaixo,baixo[1]);
+        }
+        element.innerText = novoAcorde;
+    });
+}
+
+function carregaArquivo(nomeArquivo){
+    ultimaMusica = nomeArquivo;
+    tomAtual = 0;
+    fetch(`hino-32/${nomeArquivo}.html`)
+    .then(function(response) {
+        response.text().then(function(text) {  
+        transformar(text);
+        });
+    });
+}
+
+function abrirMusica(event){
+    let nome = event.target.getAttribute('data-arquivo');    
+    carregaArquivo(nome);
+}
+
+function reset() {
+    carregaArquivo(ultimaMusica);
+}
+
+function transposicaoBotao(meioTom) { // enviar 1 ou -1    
+    tomAtual = tomAtual + meioTom;    
+    document.querySelectorAll('#resultado > pre > b').forEach(element => {
+        // console.log(element.innerText);
+        let acorde = element.innerText;
+        let baixo = element.innerText.split('/');        
+        let numeroAtual = biblioteca.getNumeroBaseAcorde(acorde);
+        let numeroTranposto = numeroAtual + (meioTom);        
+        if(numeroTranposto > 12)
+            numeroTranposto = 1;
+        if(numeroTranposto < 1)
+            numeroTranposto = 12;        
+        let novaBase = biblioteca.getBaseAcordePorPosicao(numeroTranposto);
+        let novoAcorde = biblioteca.alterarBaseAcorde(novaBase,baixo[0]);
+        // alterar baixo
+        if(baixo.length > 1){
+            let numeroAtualBaixo = biblioteca.getNumeroBaseAcorde(baixo[1]);
+            let numeroTranpostoBaixo = numeroAtualBaixo + (meioTom);        
+            if(numeroTranpostoBaixo > 12)
+                numeroTranpostoBaixo = 1;
+            if(numeroTranpostoBaixo < 1)
+                numeroTranpostoBaixo = 12;  
+            let novaBaseBaixo = biblioteca.getBaseAcordePorPosicao(numeroTranpostoBaixo);
+            novoAcorde = novoAcorde + '/' + biblioteca.alterarBaseAcorde(novaBaseBaixo,baixo[1]);
+        }
+        element.innerText = novoAcorde;
+    });
+
+
+
+    var tempo;
+function rolar(e){
+   if(e){
+      var el = document.querySelector("#conteudo");
+      tempo = setInterval(function(){
+         var doc_scrl = document.documentElement.scrollTop;
+         
+         if(el.offsetTop-doc_scrl <= window.innerHeight-el.offsetHeight){ // aqui é quando a janela chega ao fim
+            clearInterval(tempo);
+         }else{
+            window.scroll(0,doc_scrl+2);
+         }
+      }, 500);
+
+   }else{
+      clearInterval(tempo);
+   }
+}
+}
---
+++ projeto igreja/shekinah-kadoshi/meu-site/videos-youtube/videos.js
@@ -0,0 +1,138 @@
+const videoYtbLazy = document.getElementById('videoYtbLazy');
+
+videoYtbLazy.addEventListener('click',snippetYtbLazyPlay)
+
+function snippetYtbLazyPlay(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazyPlay);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy1 = document.getElementById('videoYtbLazy1');
+
+videoYtbLazy1.addEventListener('click',snippetYtbLazy1Play)
+
+function snippetYtbLazy1Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy1Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy2 = document.getElementById('videoYtbLazy2');
+
+videoYtbLazy2.addEventListener('click',snippetYtbLazy2Play)
+
+function snippetYtbLazy2Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy3Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy3 = document.getElementById('videoYtbLazy3');
+
+videoYtbLazy3.addEventListener('click',snippetYtbLazy3Play)
+
+function snippetYtbLazy3Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy3Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy4 = document.getElementById('videoYtbLazy4');
+
+videoYtbLazy4.addEventListener('click',snippetYtbLazy4Play)
+
+function snippetYtbLazy4Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy4Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy5 = document.getElementById('videoYtbLazy5');
+
+videoYtbLazy5.addEventListener('click',snippetYtbLazy5Play)
+
+function snippetYtbLazy5Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy5Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy6 = document.getElementById('videoYtbLazy6');
+
+videoYtbLazy6.addEventListener('click',snippetYtbLazy6Play)
+
+function snippetYtbLazy6Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy6Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy7 = document.getElementById('videoYtbLazy7');
+
+videoYtbLazy7.addEventListener('click',snippetYtbLazy7Play)
+
+function snippetYtbLazy7Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy7Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy8 = document.getElementById('videoYtbLazy8');
+
+videoYtbLazy8.addEventListener('click',snippetYtbLazy8Play)
+
+function snippetYtbLazy8Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy8Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy9 = document.getElementById('videoYtbLazy9');
+
+videoYtbLazy9.addEventListener('click',snippetYtbLazy9Play)
+
+function snippetYtbLazy9Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy9Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy10 = document.getElementById('videoYtbLazy10');
+
+videoYtbLazy10.addEventListener('click',snippetYtbLazy10Play)
+
+function snippetYtbLazy10Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy10Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy11 = document.getElementById('videoYtbLazy11');
+
+videoYtbLazy11.addEventListener('click',snippetYtbLazy11Play)
+
+function snippetYtbLazy11Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy11Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy12 = document.getElementById('videoYtbLazy12');
+
+videoYtbLazy12.addEventListener('click',snippetYtbLazy12Play)
+
+function snippetYtbLazy12Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy12Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy13 = document.getElementById('videoYtbLazy13');
+
+videoYtbLazy13.addEventListener('click',snippetYtbLazy13Play)
+
+function snippetYtbLazy13Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy13Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy14 = document.getElementById('videoYtbLazy14');
+
+videoYtbLazy14.addEventListener('click',snippetYtbLazy14Play)
+
+function snippetYtbLazy14Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy14Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy15 = document.getElementById('videoYtbLazy15');
+
+videoYtbLazy15.addEventListener('click',snippetYtbLazy15Play)
+
+function snippetYtbLazy15Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy15Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+const videoYtbLazy16 = document.getElementById('videoYtbLazy16');
+
+videoYtbLazy16.addEventListener('click',snippetYtbLazy16Play)
+
+function snippetYtbLazy16Play(evt) {
+evt.currentTarget.removeEventListener('click', snippetYtbLazy16Play);
+evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
+
+
